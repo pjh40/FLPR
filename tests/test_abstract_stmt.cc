b3772dfdbcc8b3250c7df09032bcddc351f42797
Paul Henning <phenning@lanl.gov>
2019-09-26 14:39:23 -0600

    Added more tests for AST::function_stmt

--- tests/test_abstract_stmt.cc
+++ tests/test_abstract_stmt.cc
@@ -23,9 +23,40 @@ using namespace FLPR;
     FLPR::Stmt::Stmt_Tree st = FLPR::Stmt::SP(ts);                      \
     FLPR::AST::AS_Tree ast = FLPR::AST::SP(st.cursor())                          
 
+#define TAG(X) Syntax_Tags::X
 
 bool function_stmt() {
-  AS_STMT(function_stmt, "function foo(a)");
+  {
+    AS_STMT(function_stmt, "function foo(a,b)");
+    TEST_INT(ast.size(), 7);
+    auto c = ast.ccursor();
+    TEST_INT(c.num_branches(), 4);
+    c.down();
+
+    TEST_TOK(SG_PREFIX, c->syntag());
+    TEST_FALSE(c->has_stmt_cursor());
+    TEST_INT(c.num_branches(), 0);
+    c.next();
+
+    TEST_TOK(TK_NAME, c->syntag());
+    TEST_TRUE(c->has_stmt_cursor());
+    TEST_TOK(TK_NAME, c->stmt_cursor()->syntag);
+    TEST_INT(c->stmt_cursor()->token_range.size(), 1);
+    TEST_STR("foo", c->front_tt().text());
+    TEST_INT(c.num_branches(), 0);
+    c.next();
+
+    TEST_TOK(SG_DUMMY_ARG_NAME_LIST, c->syntag());
+    TEST_INT(c.num_branches(), 2);
+    c.down();
+    TEST_STR("a", c->front_tt().text());
+    c.next();
+    TEST_STR("b", c->front_tt().text());
+    c.up();
+    
+    c.next();
+    TEST_TOK(SG_SUFFIX, c->syntag());
+  }
   return true;
 }
 
