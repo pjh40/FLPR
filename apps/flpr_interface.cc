2ff162bb356f96b3323fc2d013d29e0473ce5a39
Paul Henning <phenning@lanl.gov>
2019-10-07 11:01:07 -0600

    Added skeleton of output capability.

--- apps/flpr_interface.cc
+++ apps/flpr_interface.cc
@@ -8,15 +8,16 @@
    BSD-3 License can be found in the LICENSE file of the repository.
 */
 
-/*
+/*!
   \file flpr_interface.cc
 
   Extract interface definitions for top-level subroutines and functions
 */
 
-#include "flpr/flpr.hh"
 #include "flpr/astruct.hh"
+#include "flpr/flpr.hh"
 #include <iostream>
+#include <sstream>
 
 #define TAG(X) FLPR::Syntax_Tags::X
 
@@ -32,8 +33,22 @@ using Procedure = FLPR::Procedure<File>;
 
 class Interface_Action {
 public:
+  using Line_Buf = FLPR::Logical_File::Line_Buf;
+
+public:
+  void append_line(char const *const text) {
+    output_buf_.emplace_back(std::string{text});
+  }
+  void append_line(std::string const &text) { output_buf_.push_back(text); }
+  void append_line(std::string &&text) { output_buf_.emplace_back(text); }
+
+  constexpr Line_Buf const &buf() const { return output_buf_; }
+
   bool operator()(File &file, Cursor c, bool const internal_procedure,
                   bool const module_procedure) const;
+
+private:
+  Line_Buf output_buf_;
 };
 
 bool do_file(std::string const &filename, FLPR::File_Type file_type,
@@ -41,22 +56,36 @@ bool do_file(std::string const &filename, FLPR::File_Type file_type,
 
 } // namespace FLPR_Interface
 
-
-
 int main(int argc, char *argv[]) {
-  if(argc != 2) {
+  if (argc != 2) {
     std::cerr << "Usage: flpr_interface <fortran file>" << std::endl;
     return 1;
   }
-  
-  std::string const filename {argv[1]};
+
+  char const module_name[] = "foo";
+  const std::string modstr{std::string{"module "} + std::string{module_name}};
+
+  std::string const filename{argv[1]};
   FLPR_Interface::Interface_Action interface;
+  interface.append_line(modstr);
+  interface.append_line("interface");
+
   do_file(filename, FLPR::file_type_from_extension(filename), interface);
-  
+
+  interface.append_line("end interface");
+  interface.append_line("end" + modstr);
+
+  FLPR::Parsed_File output_file(interface.buf(), "output_name.f90",
+                                FLPR::File_Type::FREEFMT);
+  FLPR::Indent_Table indents;
+  indents.apply_emacs_indent();
+  output_file.indent(indents);
+  for (auto const &ll : output_file.logical_lines()) {
+    std::cout << ll;
+  }
 
   return 0;
 }
-
 
 namespace FLPR_Interface {
 
@@ -76,7 +105,6 @@ bool do_file(std::string const &filename, FLPR::File_Type file_type,
 
 /*--------------------------------------------------------------------------*/
 
-
 bool Interface_Action::operator()(File &file, Cursor c,
                                   bool const internal_procedure,
                                   bool const module_procedure) const {
@@ -90,42 +118,38 @@ bool Interface_Action::operator()(File &file, Cursor c,
     return false;
   }
 
-  if(proc.is_main_program()) {
+  if (proc.is_main_program()) {
     return false;
   }
 
   auto proc_stmt = proc.cbegin(Procedure::PROC_BEGIN);
   assert(std::next(proc_stmt) == proc.cend(Procedure::PROC_BEGIN));
-  auto stmt_cursor {proc_stmt->stmt_tree().ccursor()};
+  auto stmt_cursor{proc_stmt->stmt_tree().ccursor()};
   int stmt_syntag = stmt_cursor->syntag;
-  
-  std::vector<std::string> dummy_names;  
-  switch(stmt_syntag) {
-  case TAG(SG_SUBROUTINE_STMT):
-    {
-    }
-    break;
-  case TAG(SG_FUNCTION_STMT):
-    {
-      std::optional<FLPR::AST::Function_Stmt> ast =
+
+  std::vector<std::string> dummy_names;
+  switch (stmt_syntag) {
+  case TAG(SG_SUBROUTINE_STMT): {
+  } break;
+  case TAG(SG_FUNCTION_STMT): {
+    std::optional<FLPR::AST::Function_Stmt> ast =
         FLPR::AST::Function_Stmt::ingest(stmt_cursor);
-      assert(ast.has_value());
-      std::cout << "FUNCTION " << ast->name->token_range.front() << ": ";
-      for(auto c : ast->dummy_arg_name_list) {
-        std::cout << c->token_range.front() << ' ';
-      }
-      std::cout << '\n';
+    assert(ast.has_value());
+    std::cout << "FUNCTION " << ast->name->token_range.front() << ": ";
+    for (auto c : ast->dummy_arg_name_list) {
+      std::cout << c->token_range.front() << ' ';
     }
-    break;
+    std::cout << '\n';
+  } break;
   default:
     std::cerr << "Got unexpected procedure statement of "
               << FLPR::Syntax_Tags::label(stmt_syntag) << std::endl;
   }
-  for(auto const &n : dummy_names) {
+  for (auto const &n : dummy_names) {
     std::cout << ' ' << n;
   }
   std::cout << '\n';
-  
+
   return true;
 }
-}
+} // namespace FLPR_Interface
